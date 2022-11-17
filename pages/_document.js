38679c2f0aa490aeb31d85e56e19329fe2a9d7a4
Olive <olivelliott48@gmail.com>
2022-11-16 20:41:03 -0500

    Troubleshoot home page & navbar migration to app dir

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link
           href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap"
           rel="stylesheet"
