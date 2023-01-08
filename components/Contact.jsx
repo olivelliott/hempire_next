f8cc3c3032f60f9769b5d12736f35aed49153b25
Olive <olivelliott48@gmail.com>
2023-01-08 17:06:45 -0500

    Update mobile styling for contact component

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -19,22 +19,22 @@ const Contact = () => {
           layout='responsive'
         />
 
-        <div className="absolute inset-0 p-4 text-center my-18 md:my-32 lg:my-20">
-          <h1 className="text-4xl md:text-6xl lg:text-6xl font-sans font-bold text-center p-4 text-white uppercase">
+        <div className="absolute inset-0 p-4 text-center my-24 lg:my-20">
+          <h1 className="text-8xl lg:text-8xl font-sans font-bold text-center p-4 text-white uppercase">
             connect with us
           </h1>
           <div className="flex justify-around mt-4 pb-4">
             <a href='https://www.instagram.com/hempirerocks/' target='_blank' rel="noreferrer">
               <AiOutlineInstagram
-                size={33}
-                className="relative ease-in duration-300 hover:-translate-y-1 hover:scale-110 text-secondary"
+                size={75}
+                className="relative ease-in duration-300 hover:-translate-y-1 hover:scale-110 text-secondary w-50 h-50"
                 width={35}
                 height={35}
               />
             </a>
             <a href='https://www.facebook.com/hempirerocks/' target='_blank' rel="noreferrer">
               <FaFacebookSquare
-                size={31}
+                size={71}
                 className="relative ease-in duration-300 hover:-translate-y-1 hover:scale-110 text-secondary"
                 width={35}
                 height={35}
@@ -42,15 +42,15 @@ const Contact = () => {
             </a>
             <a href='https://hempirerocks.bandcamp.com/' target='_blank' rel="noreferrer">
               <FaBandcamp
-                size={30}
+                size={70}
                 className="relative ease-in duration-300 hover:-translate-y-1 hover:scale-110 text-secondary"
               />
             </a>
           </div>
-          <p className="text-xl lg:text-3xl mt-4 font-bold uppercase text-white font-sans">or</p>
+          <p className="text-6xl lg:text-3xl mt-4 font-bold uppercase text-white font-sans">or</p>
           <a
             href="mailto:jkshaw13@gmail.com"
-            className=" font-sans btn btn-sm md:btn-md lg:btn-md border-none relative ease-in duration-300 mt-4 text-sm rounded bg-neutral active:bg-gray-800 text-white text-center active:text-gray-200"
+            className=" font-sans btn btn-lg lg:btn-md border-none relative ease-in duration-300 mt-8 text-3xl rounded bg-neutral active:bg-gray-800 text-white text-center active:text-gray-200"
           >
             send an email
           </a>
