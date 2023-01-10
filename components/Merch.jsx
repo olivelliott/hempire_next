0cdafbc9ca9ecaf2e48c7a295237a0943e7987e3
Olive <olivelliott48@gmail.com>
2023-01-09 20:20:28 -0500

    Update media query for md: screens on merch component

--- components/Merch.jsx
+++ components/Merch.jsx
@@ -133,7 +133,7 @@ const Merch = () => {
           merch
         </h1>
 
-        <div className="grid gap-y-10 gap-x-6 grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 mt-8 pt-2">
+        <div className="grid gap-y-10 gap-x-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 mt-8 pt-2">
           {product.map((product) => (
             <a key={product.id} href={product.href} className="group">
               <div className="relative">
@@ -158,10 +158,10 @@ const Merch = () => {
                   </div>
                 </div>
               </div>
-              <h3 className="mt-4 text-5xl lg:text-2xl text-white font-sans">
+              <h3 className="mt-4 text-5xl md:text-2xl md:mt-1 lg:text-2xl text-white font-sans">
                 {product.name}
               </h3>
-              <p className="mt-3 text-4xl lg:text-xl lg:mt-0 font-medium text-secondary font-sans">
+              <p className="mt-3 text-4xl md:text-xl md:mt-0 lg:text-xl lg:mt-0 font-medium text-secondary font-sans">
                 {product.price}
               </p>
             </a>
@@ -199,7 +199,7 @@ const Merch = () => {
                   leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
                 >
                   <Dialog.Panel className="flex w-full transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
-                    <div className="relative flex w-full items-center overflow-hidden bg-white px-4 pt-14 pb-8 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                    <div className="relative flex w-full items-center overflow-hidden bg-gray-900 px-4 pt-14 pb-8 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
                       <button
                         type="button"
                         className="absolute top-4 right-4 text-gray-400 hover:text-gray-500 sm:top-8 sm:right-6 md:top-6 md:right-6 lg:top-8 lg:right-8"
@@ -219,7 +219,7 @@ const Merch = () => {
                           />
                         </div>
                         <div className="sm:col-span-8 lg:col-span-7">
-                          <h2 className="text-2xl font-bold text-gray-900 sm:pr-12">
+                          <h2 className="text-2xl font-bold text-white sm:pr-12">
                             {selectedProduct.name}
                           </h2>
 
@@ -231,7 +231,7 @@ const Merch = () => {
                               Product information
                             </h3>
 
-                            <p className="text-2xl text-gray-900">
+                            <p className="text-2xl text-white">
                               {selectedProduct.price}
                             </p>
 
@@ -267,12 +267,12 @@ const Merch = () => {
                                           className={({ active }) =>
                                             classNames(
                                               size.inStock
-                                                ? "bg-white shadow-sm text-gray-900 cursor-pointer"
-                                                : "bg-gray-50 text-gray-200 cursor-not-allowed",
+                                                ? "bg-gray-600 shadow-sm text-white cursor-pointer"
+                                                : "bg-gray-650 text-gray-200 cursor-not-allowed",
                                               active
-                                                ? "ring-2 ring-indigo-500"
+                                                ? "ring-secondary"
                                                 : "",
-                                              "group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1"
+                                              "group relative border border-gray-600 rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-500 focus:outline-none sm:flex-1"
                                             )
                                           }
                                         >
@@ -288,7 +288,7 @@ const Merch = () => {
                                                       ? "border"
                                                       : "border-2",
                                                     checked
-                                                      ? "border-indigo-500"
+                                                      ? "border-tertiary"
                                                       : "border-transparent",
                                                     "pointer-events-none absolute -inset-px rounded-md"
                                                   )}
@@ -326,7 +326,7 @@ const Merch = () => {
 
                               <button
                                 type="submit"
-                                className="mt-6 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                className="mt-6 flex w-full items-center justify-center rounded-md border border-transparent bg-secondary py-3 px-8 text-base font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
                               >
                                 Add to bag
                               </button>
