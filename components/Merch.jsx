347fe3716e2b6a5dca257b769f7cb00c9640d7d5
Olive <olivelliott48@gmail.com>
2023-01-09 20:05:40 -0500

    Fix sizing for merch products

--- components/Merch.jsx
+++ components/Merch.jsx
@@ -58,6 +58,10 @@ const product = [
       { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
       { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
     ],
+    sizes: [
+      { name: "OS", inStock: true },
+    ],
+
   },
   {
     id: 2,
@@ -71,6 +75,10 @@ const product = [
       { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
       { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
     ],
+    sizes: [
+      { name: "OS", inStock: true },
+    ],
+
   },
   {
     id: 3,
@@ -96,7 +104,6 @@ const product = [
       { name: "XXXL", inStock: false },
     ],
   },
-  // More products...
 ];
 
 function classNames(...classes) {
@@ -221,7 +228,7 @@ const Merch = () => {
                             className="mt-2"
                           >
                             <h3 id="information-heading" className="sr-only">
-                              selectedProduct information
+                              Product information
                             </h3>
 
                             <p className="text-2xl text-gray-900">
@@ -238,8 +245,8 @@ const Merch = () => {
                                 Size guide
                               </a>
                             </div>
- */}
-                                     {/* <p>{selectedProduct}.sizes[0].name</p> */}
+
+                                     <p>{selectedProduct.sizes}</p> */}
 
                                 <RadioGroup
                                   value={selectedSize}
@@ -251,66 +258,68 @@ const Merch = () => {
                                     Choose a size{" "}
                                   </RadioGroup.Label>
                                   <div className="grid grid-cols-4 gap-4">
-                                    {sizes.map((size) => (
-                                      <RadioGroup.Option
-                                        key={size.name}
-                                        value={size.sizes}
-                                        disabled={!size.inStock}
-                                        className={({ active }) =>
-                                          classNames(
-                                            size.inStock
-                                              ? "bg-white shadow-sm text-gray-900 cursor-pointer"
-                                              : "bg-gray-50 text-gray-200 cursor-not-allowed",
-                                            active
-                                              ? "ring-2 ring-indigo-500"
-                                              : "",
-                                            "group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1"
-                                          )
-                                        }
-                                      >
-                                        {({ active, checked }) => (
-                                          <>
-                                            <RadioGroup.Label as="span">
-                                              {size.name}
-                                            </RadioGroup.Label>
-                                            {size.inStock ? (
-                                              <span
-                                                className={classNames(
-                                                  active
-                                                    ? "border"
-                                                    : "border-2",
-                                                  checked
-                                                    ? "border-indigo-500"
-                                                    : "border-transparent",
-                                                  "pointer-events-none absolute -inset-px rounded-md"
-                                                )}
-                                                aria-hidden="true"
-                                              />
-                                            ) : (
-                                              <span
-                                                aria-hidden="true"
-                                                className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"
-                                              >
-                                                <svg
-                                                  className="absolute inset-0 h-full w-full stroke-2 text-gray-200"
-                                                  viewBox="0 0 100 100"
-                                                  preserveAspectRatio="none"
-                                                  stroke="currentColor"
+                                  
+                                      {selectedProduct.sizes.map((size) => (
+                                        <RadioGroup.Option
+                                          key={size.name}
+                                          value={size}
+                                          disabled={!size.inStock}
+                                          className={({ active }) =>
+                                            classNames(
+                                              size.inStock
+                                                ? "bg-white shadow-sm text-gray-900 cursor-pointer"
+                                                : "bg-gray-50 text-gray-200 cursor-not-allowed",
+                                              active
+                                                ? "ring-2 ring-indigo-500"
+                                                : "",
+                                              "group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1"
+                                            )
+                                          }
+                                        >
+                                          {({ active, checked }) => (
+                                            <>
+                                              <RadioGroup.Label as="span">
+                                                {size.name}
+                                              </RadioGroup.Label>
+                                              {size.inStock ? (
+                                                <span
+                                                  className={classNames(
+                                                    active
+                                                      ? "border"
+                                                      : "border-2",
+                                                    checked
+                                                      ? "border-indigo-500"
+                                                      : "border-transparent",
+                                                    "pointer-events-none absolute -inset-px rounded-md"
+                                                  )}
+                                                  aria-hidden="true"
+                                                />
+                                              ) : (
+                                                <span
+                                                  aria-hidden="true"
+                                                  className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"
                                                 >
-                                                  <line
-                                                    x1={0}
-                                                    y1={100}
-                                                    x2={100}
-                                                    y2={0}
-                                                    vectorEffect="non-scaling-stroke"
-                                                  />
-                                                </svg>
-                                              </span>
-                                            )}
-                                          </>
-                                        )}
-                                      </RadioGroup.Option>
-                                    ))}
+                                                  <svg
+                                                    className="absolute inset-0 h-full w-full stroke-2 text-gray-200"
+                                                    viewBox="0 0 100 100"
+                                                    preserveAspectRatio="none"
+                                                    stroke="currentColor"
+                                                  >
+                                                    <line
+                                                      x1={0}
+                                                      y1={100}
+                                                      x2={100}
+                                                      y2={0}
+                                                      vectorEffect="non-scaling-stroke"
+                                                    />
+                                                  </svg>
+                                                </span>
+                                              )}
+                                            </>
+                                          )}
+                                        </RadioGroup.Option>
+                                      ))}
+                                  
                                   </div>
                                 </RadioGroup>
                               </div>
